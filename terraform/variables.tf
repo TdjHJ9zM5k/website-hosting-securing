fbb0e5fb91f451ec4f14651e6d5357dc91ae2d8a
Davide A. <procioni.birboni@gmail.com>
2024-11-27 17:31:31 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+}
+
+variable "main_bucket_name" {
+  description = "The name of the main bucket"
+  type        = string
+}
+
+variable "bucket_zone" {
+  description = "The location/zone of the buckets"
+  type        = string
+}
